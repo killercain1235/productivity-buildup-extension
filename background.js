9fd5140eb0fb9b44bc070cc416641e61d8fcd430
vinantichavan <86634464+vinantichavan@users.noreply.github.com>
2021-07-15 19:57:45 +0530

    Update background.js

--- background.js
+++ background.js
@@ -93,34 +93,3 @@ chrome.tabs.onUpdated.addListener(function () {
 
 
 
-// Alarm Clock 
-
-// listener for alarm, execute when an alarm has elasped
-chrome.alarms.onAlarm.addListener((alarm) => {
-    console.log("alarm", alarm);
-    chrome.notifications.create({
-        type: 'basic',
-        iconUrl: 'notification32.png',
-        title: 'Alarm',
-        message: alarm.name,
-        buttons: [{
-            title: 'Keep it Flowing.'
-        }],
-        priority: 0
-    });
-    const audio = new Audio('./assets/calm_morning_alarm.mp3');
-    audio.play();
-})
-
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//     console.log('tabId, changeInfo, tab', tabId, changeInfo, tab);
-//     chrome.storage.sync.get('alarmData', (storage) => {
-//         console.log('storage', storage);
-
-//         if (storage.alarmData.length) {
-
-//         } else {
-
-//         }
-//     });
-// })
